127f246066af271626301d4db4ef94fb08719e07
Lionel Vaskó <2003lionel@gmail.com>
2025-04-21 15:59:03 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Dolgozó_nyilvántartó
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
